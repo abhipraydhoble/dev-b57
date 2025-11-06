d64a2bf26ee76031c68046a7f81eed6fcbd8d6fa
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-11-06 11:32:01 +0530

    Create variable.tf

---
+++ Devops/Terraform/Variables/v2/variable.tf
@@ -0,0 +1,34 @@
+variable "vpc_cidr_block" {
+  type = string 
+  description = "value of vpc cidr block"
+}
+
+variable "subnet_cidr_block" {
+  type = string 
+}
+
+variable "az" {
+  type = string 
+ 
+}
+
+variable "public_ip" {
+  type = bool 
+  
+}
+
+
+variable "ami_id" {
+  type = string 
+
+}
+
+variable "ins_type" {
+ type = string
+
+}
+
+variable "key" {
+  type = string 
+
+}
