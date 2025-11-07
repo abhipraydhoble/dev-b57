8aeb1613f1ad8c65fa543d0f337159f362911ff0
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-11-07 11:32:45 +0530

    Create ec2-variable.tf

---
+++ Devops/Terraform/Modules/easy/tf-modules/ec2/ec2-variable.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "ins_type" {
+  type = string 
+}
+
+variable "key" {
+  type = string
+}
+
+
+
+#additional variables
+variable "subnet_id" {
+  type = string
+}
+
+
+variable "security_group_id" {
+ type = string 
+}
